eb160911910221aab2c2f7a8ce3e8e97c53968c0
max-kubasov <106583221+max-kubasov@users.noreply.github.com>
2024-05-26 12:17:14 +0300

    Add extension for URLSession

---
+++ MVVM/Extension/URLSession + Extension .swift
@@ -0,0 +1,20 @@
+//
+//  URLSession + Extension .swift
+//  MVVM
+//
+//  Created by Max on 26.05.2024.
+//
+
+import Foundation
+
+extension URLSession {
+    
+    typealias Handler = (Data?, URLResponse?, Error?) -> Void
+    
+    //@discardableResult
+    func request(_ endPoint: EndPoint, handler: @escaping Handler) -> URLSessionDataTask {
+        let task = dataTask(with: endPoint.url, completionHandler: handler)
+        task.resume()
+        return task
+    }
+}
