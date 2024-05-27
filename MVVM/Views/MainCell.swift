879a343cb7e09139144645d881b12e21f7d5f737
max-kubasov <106583221+max-kubasov@users.noreply.github.com>
2024-05-27 21:49:50 +0300

    Setup custom cell

--- MVVM/Views/MainCell.swift
+++ MVVM/Views/MainCell.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class MainCell: UITableViewCell {
     
+    private var nameLabel = UILabel()
+    private var emailLabel = UILabel()
+    private var labelStackView = UIStackView()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setupView() {
+        nameLabel.font = .systemFont(ofSize: 16)
+        emailLabel.font = .systemFont(ofSize: 14)
+        labelStackView = UIStackView(arrangedSubviews: [nameLabel, emailLabel])
+        
+        labelStackView.axis = .vertical
+        labelStackView.distribution = .fillEqually
+        labelStackView.spacing = 4
+        labelStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(labelStackView)
+        
+        NSLayoutConstraint.activate([
+            labelStackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            labelStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20)
+        ])
     }
 }
