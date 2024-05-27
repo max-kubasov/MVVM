0c728440be9af63e4800ea7b82328d579fe25664
max-kubasov <106583221+max-kubasov@users.noreply.github.com>
2024-05-27 21:34:07 +0300

    Add custom cell

---
+++ MVVM/Views/MainCell.swift
@@ -0,0 +1,19 @@
+//
+//  MainCell.swift
+//  MVVM
+//
+//  Created by Max on 27.05.2024.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
